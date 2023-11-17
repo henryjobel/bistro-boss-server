77cd89b0ce82851cbf905972c7eb942ac9ecec44
henryjobel <jhobelsaha@gmail.com>
2023-11-18 00:53:40 +0600

    jwt token creayed

--- index.js
+++ index.js
@@ -90,18 +90,18 @@ async function run() {
       const result = await userCollection.find().toArray()
       res.send(result)
   })
-    app.post('/users' , async(req,res)=>{
-      const user = req.body;
-
-      const query = {email: user.email}
-      const existingUser = await userCollection.find(query)
-      if(existingUser){
-        return res.send({message: 'user already exisit', insertedId: null})
-      }
-
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    })
+  app.post('/users', async (req, res) => {
+    const user = req.body;
+    // insert email if user doesnt exists: 
+    // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+    const query = { email: user.email }
+    const existingUser = await userCollection.findOne(query);
+    if (existingUser) {
+      return res.send({ message: 'user already exists', insertedId: null })
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  });
 
 
 
