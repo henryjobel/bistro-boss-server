4034f51c4c95ad2d977873592362dcac15ce8d5b
henryjobel <jhobelsaha@gmail.com>
2023-11-20 01:15:17 +0600

    get post added

--- index.js
+++ index.js
@@ -77,6 +77,11 @@ async function run() {
         const result = await menuCollection.find().toArray()
         res.send(result)
     })
+    app.post('/menu', verifyToken,verifyAdmin, async(req,res) =>{
+        const item = req.body
+        const result = await menuCollection.insertOne(item)
+        res.send(result)
+    })
     app.get('/review', async(req,res) =>{
         const result = await reviewsCollection.find().toArray()
         res.send(result)
